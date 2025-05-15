3f634961dacef909aa1f8ec598a8024aeda66aab
Hanh <hanh.phan7891@gmail.com>
2025-05-15 13:35:54 +0700

    Update new code

--- src/BaiTapState/BTState.jsx
+++ src/BaiTapState/BTState.jsx
@@ -1,168 +1,69 @@
 import React, { useState } from "react";
+import dataGlasses from "../../dataGlasses.json";
 
 const BTState = () => {
-  const [glassesList, setGlassesList] = useState("./public/img/v1.png");
+  const [selectedGlasses, setSelectedGlasses] = useState(dataGlasses[0]);
 
   const handleChangeGlasses = (glasses) => {
-    let img = `./public/img/${glasses}.png`;
-    setGlassesList(img);
+    setSelectedGlasses(glasses);
   };
+
   return (
-    <div>
-      <div className="backgroundIMG">
-        <img class="card-img-top" src="./public/img/background.jpg" alt="" />
-        <div className="pageContent">
+    <div className="backgroundIMG">
+      <img
+        className="card-img-top"
+        src="./public/glassesImage/background.jpg"
+        alt="background"
+      />
+      <div className="pageContent">
+        {/* Header */}
+        <div
+          className="container-fluid  d-flex justify-content-center align-items-center"
+          style={{ height: "80px" }}
+        >
+          <span className="navbar-brand mb-0 text-light fs-2">
+            TRY GLASSES APP ONLINE
+          </span>
+        </div>
+
+        {/* Model & Info */}
+        <div>
           <div
-            className="container-fluid bg-dark d-flex justify-content-center align-items-center"
-            style={{ height: "80px" }}
+            className="glassesModel mt-5 d-flex  "
+            style={{ width: "340px", height: "400px" }}
           >
-            <span className="navbar-brand mb-0 h1 text-light fs-2">
-              TRY GLASSES APP ONLINE
-            </span>
-          </div>
+            <img src="./public/glassesImage/model.jpg" alt="Model" />
 
-          <div>
-            <div className="glassesModel">
-              <img src="./public/img/model.jpg" alt="Model" />
-
-              <img
-                className="glassesPicture"
-                src={glassesList}
-                alt="Glasses picture"
-              />
-              <div
-                className="bg px-2"
-                style={{ width: "480px", height: "150px" }}
-              >
-                <h3 className="title-text pt-3">FENDI F8750</h3>
-                <p className="text-light">
-                  Light pink square lenses define these sunglasses, ending with
-                  amother of pearl effect tip.
-                </p>
-              </div>
+            <img
+              className="glassesPicture"
+              src={selectedGlasses.url}
+              alt="Selected glasses"
+            />
+            <div className="bg px-2 py-1 ">
+              <h3 className="title-text ">{selectedGlasses.name}</h3>
+              <p className="text-light">{selectedGlasses.desc}</p>
             </div>
           </div>
 
+          {/* List kính */}
           <div
-            className=" bg-primary-subtle mt-5 mx-auto"
-            style={{
-              width: "800px",
-              height: "180px",
-              backgroundColor: "white",
-            }}
+            className="bg-secondary-subtle mt-5 mx-auto"
+            style={{ width: "800px", height: "160px" }}
           >
-            <div className="my-auto">
-              <button
-                className="btn btn-outline-primary"
-                onClick={() => {
-                  handleChangeGlasses("v2");
-                }}
-              >
-                <img
-                  className=""
-                  style={{ width: "120px", height: "60px" }}
-                  src="./public/img/v2.png"
-                  alt=""
-                />
-              </button>
-
-              <button
-                className="btn btn-outline-secondary"
-                onClick={() => {
-                  handleChangeGlasses("v3");
-                }}
-              >
-                <img
-                  className=""
-                  style={{ width: "120px", height: "60px" }}
-                  src="./public/img/v3.png"
-                  alt=""
-                />
-              </button>
-
-              <button
-                className="btn btn-outline-success"
-                onClick={() => {
-                  handleChangeGlasses("v4");
-                }}
-              >
-                <img
-                  className=""
-                  style={{ width: "120px", height: "60px" }}
-                  src="./public/img/v4.png"
-                  alt=""
-                />
-              </button>
-
-              <button
-                className="btn btn-outline-danger"
-                onClick={() => {
-                  handleChangeGlasses("v5");
-                }}
-              >
-                <img
-                  className=""
-                  style={{ width: "120px", height: "60px" }}
-                  src="./public/img/v5.png"
-                  alt=""
-                />
-              </button>
-
-              <button
-                className="btn btn-outline-warning"
-                onClick={() => {
-                  handleChangeGlasses("v6");
-                }}
-              >
-                <img
-                  className=""
-                  style={{ width: "120px", height: "60px" }}
-                  src="./public/img/v6.png"
-                  alt=""
-                />
-              </button>
-
-              <button
-                className="btn btn-outline-info"
-                onClick={() => {
-                  handleChangeGlasses("v7");
-                }}
-              >
-                <img
-                  className=""
-                  style={{ width: "120px", height: "60px" }}
-                  src="./public/img/v7.png"
-                  alt=""
-                />
-              </button>
-
-              <button
-                className="btn btn-outline-primary"
-                onClick={() => {
-                  handleChangeGlasses("v8");
-                }}
-              >
-                <img
-                  className=""
-                  style={{ width: "120px", height: "60px" }}
-                  src="./public/img/v8.png"
-                  alt=""
-                />
-              </button>
-
-              <button
-                className="btn btn-outline-secondary"
-                onClick={() => {
-                  handleChangeGlasses("v9");
-                }}
-              >
-                <img
-                  className=""
-                  style={{ width: "120px", height: "60px" }}
-                  src="./public/img/v9.png"
-                  alt=""
-                />
-              </button>
+            <div className="d-flex flex-wrap justify-content-center align-items-center gap-2 py-3">
+              {dataGlasses.map((glasses) => (
+                <button
+                  key={glasses.id}
+                  className="btn btn-outline-secondary"
+                  onClick={() => handleChangeGlasses(glasses)}
+                >
+                  <img
+                    src={glasses.url}
+                    alt={glasses.name}
+                    style={{ width: "120px", height: "45px" }}
+                  />
+                </button>
+              ))}
             </div>
           </div>
         </div>
